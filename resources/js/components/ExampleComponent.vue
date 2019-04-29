811b40eccbff09f59d8261af93456897bccfc764
Daniyal Javani <daniyal.javani@gmail.com>
2019-04-29 17:19:27 +0430

    init

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div id="people">
+            sdfsafasfd
+          <v-client-table :data="tableData" :columns="columns" :options="options" :name="namest"></v-client-table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data: function() {
+                return {
+                    columns: ['id', 'name', 'age'],
+                    tableData: [
+                        { id: 1, name: "John", age: "20" },
+                        { id: 2, name: "Jane", age: "24" },
+                        { id: 3, name: "Susan", age: "16" },
+                        { id: 4, name: "Chris", age: "55" },
+                        { id: 5, name: "Dan", age: "40" }
+                    ],
+                    options: {
+                        // see the options API
+                    },
+                    namest: 'asdf'
+                }
+            },
+    }
+</script>
